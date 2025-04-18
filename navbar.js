8ea27e5b4bc61b29b0bac601499e37f3476d3164
Chr1s <42826812+chr1s0x1@users.noreply.github.com>
2025-04-18 18:50:46 -0400

    website updates

---
+++ navbar.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarPlaceholder = document.getElementById('navbar-placeholder');
+    
+    const navbar = document.createElement('nav');
+    navbar.classList.add('navbar');
+    
+    const ul = document.createElement('ul');
+    
+    const navItems = [
+    { text: 'Home', href: 'index.html' },
+    { text: 'About', href: 'about.html' },
+    { text: 'Course', href: 'itis3135/index.html' },
+    { text: 'Contract', href: 'contract.html' },
+    { text: 'Midterm', href:'itis3135/hobby/index.html'}
+    ];
+    
+    navItems.forEach(item => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = item.href;
+    a.textContent = item.text;
+    li.appendChild(a);
+    ul.appendChild(li);
+    });
+    
+    navbar.appendChild(ul);
+    navbarPlaceholder.appendChild(navbar);
+    });
