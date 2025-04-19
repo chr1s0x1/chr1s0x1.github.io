e14ffbf64ca5b6aff3049e3e4c7ee28ac4b8be52
Chr1s <42826812+chr1s0x1@users.noreply.github.com>
2025-04-19 18:44:02 -0400

    changes to about me page + more

---
+++ itis3135/navbar-course.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarPlaceholder = document.getElementById('navbar-placeholder');
+    
+    const navbar = document.createElement('nav');
+    navbar.classList.add('navbar');
+    
+    const ul = document.createElement('ul');
+    
+    const navItems = [
+    { text: 'Home', href: '../index.html' },
+    { text: 'About Me', href: '../about.html' },
+    { text: 'Course', href: 'index.html' },
+    { text: 'Contract', href: '../contract.html' },
+    { text: 'Midterm', href:'hobby/index.html'}
+    ];
+    
+    navItems.forEach(item => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = item.href;
+    a.textContent = item.text;
+    li.appendChild(a);
+    ul.appendChild(li);
+    });
+    
+    navbar.appendChild(ul);
+    navbarPlaceholder.appendChild(navbar);
+    });
